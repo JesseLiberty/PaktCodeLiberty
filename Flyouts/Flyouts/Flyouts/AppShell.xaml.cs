b88bcc145be125e0a877322fe636ec00e7024bb4
JesseLiberty <jesseliberty@gmail.com>
2020-03-21 15:20:52 -0400

    flyouts

---
+++ Flyouts/Flyouts/Flyouts/AppShell.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Flyouts.Views;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Flyouts
+{
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+
+		private void MenuItem_Clicked(object sender, EventArgs e)
+		{
+			FlyoutIsPresented = false;
+			DisplayAlert("Menu", "You clicked on a menu item", "OK");
+		}
+
+		Dictionary<string, Type> routes = new Dictionary<string, Type>();
+
+		private void RegisterRoutes()
+		{
+			routes.Add("gorilladetails", typeof(GorillaDetails));
+
+			foreach (var route in routes)
+			{
+				Routing.RegisterRoute(route.Key, route.Value);
+			}
+		}
+
+
+
+
+	}
+}
---
+++ Flyouts/Flyouts/Flyouts/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Flyouts.Models;
+
+namespace Flyouts.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
