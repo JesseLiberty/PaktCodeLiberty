73558155d0904c339c98348c72c66171b8daa3fb
JesseLiberty <jesseliberty@gmail.com>
2020-03-21 10:57:48 -0400

    First Xamarin.Forms app

---
+++ FirstXamarinFormsApp/FirstXamarinFormsApp/FirstXamarinFormsApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FirstXamarinFormsApp.Models;
+
+namespace FirstXamarinFormsApp.ViewModels
+{
+   public class ItemDetailViewModel : BaseViewModel
+   {
+      public Item Item { get; set; }
+      public ItemDetailViewModel(Item item = null)
+      {
+         Title = item?.Text;
+         Item = item;
+      }
+   }
+}
