3dfaec1278395e2914aecd99f6760015fd0d95e0
JesseLiberty <jesseliberty@gmail.com>
2020-03-31 10:33:16 -0400

    Updates for section 4

--- Animal_1/Animal_1/Animal_1.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ Animal_1/Animal_1/Animal_1.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -70,7 +70,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"a95c2928-4d22-40f5-9832-b3540d0b093b"
+	.asciz	"f6f3660f-08c1-42fa-aa9a-b8fe816411b9"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
